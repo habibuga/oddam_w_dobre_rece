1a9cfa55e9ce48fc38011b8daa726dcf72a7778e
habibuga <vitaliy.ryzhynsky@gmail.com>
2022-07-18 19:56:48 +0200

    1-model-institution

--- oddam/models.py
+++ oddam/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+ORGANIZATIONS = (
+    (1, "fundacja"),
+    (2, "organizacja pozarządowa"),
+    (3, "zbiórka lokalna"),
+)
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, verbose_name="nazwa")
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64, verbose_name="nazwa")
+    description = models.TextField(max_length=1000, verbose_name='Opis')
+    type = models.IntegerField(choices=ORGANIZATIONS, default=1, verbose_name="typ")
+    categories = models.ManyToManyField(Category, verbose_name='Kategoria')

1b7e5be7ec831a8dc2f4f9c318efcda3a53683a6
habibuga <vitaliy.ryzhynsky@gmail.com>
2022-07-18 21:03:42 +0200

    2-Strona_glowna_Dynamiczne_statystyki

--- oddam/views.py
+++ oddam/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Donation, Institution
 
 
 class LandingPage(View):
     def get(self, request):
-        return render(request=request, template_name='index.html')
+        bags = 0
+        for donation in Donation.objects.all():
+            bags += donation.quantity
+        charities = Institution.objects.count()
+        ctx = {
+            "bags_num": bags,
+            "charity_num": charities
+        }
+        return render(request=request, template_name='index.html', context=ctx)
 
 
 class AddDonation(View):

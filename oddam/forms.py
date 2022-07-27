e7db7eec907358ddac902c4dec3295254ba89397
habibuga <vitaliy.ryzhynsky@gmail.com>
2022-07-28 01:38:33 +0200

    3-Widok_rejestracji_obsługa_formularza

---
+++ oddam/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+
+class NewUserForm(UserCreationForm):
+    password1 = forms.CharField(label="Hasło", strip=False, widget=forms.PasswordInput(attrs={'placeholder': "Hasło"}), help_text=None)
+    password2 = forms.CharField(label="Powtórz hasło", strip=False, widget=forms.PasswordInput(attrs={'placeholder': "Powtórz hasło"}), help_text=None)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username")
+        help_texts = {
+            "username": None
+        }
+        widgets = {
+            "first_name": forms.TextInput(attrs={'placeholder': "Imie"}),
+            "last_name": forms.TextInput(attrs={'placeholder': "Nazwisko"}),
+            "username": forms.EmailInput(attrs={'placeholder': "Email"})
+        }
+

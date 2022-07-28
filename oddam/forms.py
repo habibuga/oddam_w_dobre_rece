5c322109ad639119e4f2736a1faf1edb35f70ea2
habibuga <vitaliy.ryzhynsky@gmail.com>
2022-07-28 02:23:21 +0200

    4-Widok_logowania

--- oddam/forms.py
+++ oddam/forms.py
@@ -6,8 +6,10 @@ User = get_user_model()
 
 
 class NewUserForm(UserCreationForm):
-    password1 = forms.CharField(label="Hasło", strip=False, widget=forms.PasswordInput(attrs={'placeholder': "Hasło"}), help_text=None)
-    password2 = forms.CharField(label="Powtórz hasło", strip=False, widget=forms.PasswordInput(attrs={'placeholder': "Powtórz hasło"}), help_text=None)
+    password1 = forms.CharField(label="Hasło", strip=False, widget=forms.PasswordInput(attrs={
+        'placeholder': "Hasło"}), help_text=None)
+    password2 = forms.CharField(label="Powtórz hasło", strip=False, widget=forms.PasswordInput(attrs={
+        'placeholder': "Powtórz hasło"}), help_text=None)
 
     class Meta:
         model = User
@@ -21,3 +23,7 @@ class NewUserForm(UserCreationForm):
             "username": forms.EmailInput(attrs={'placeholder': "Email"})
         }
 
+
+class LoginForm(forms.Form):
+    username = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Hasło'}))
